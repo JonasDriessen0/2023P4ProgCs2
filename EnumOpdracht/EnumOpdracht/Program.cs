eee1bf7fb91b6cd0bebd084b896c1324734a5b33
JonasDriessen0 <114576261+JonasDriessen0@users.noreply.github.com>
2023-05-15 12:49:05 +0200

    Enum Bruh

---
+++ EnumOpdracht/EnumOpdracht/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace EnumOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Colors myMainThemeColor = Colors.Purple;
+
+            if (myMainThemeColor == Colors.Purple)
+            {
+                Console.WriteLine("dat is mijn kleur!");
+            }
+        }
+    }
+}
