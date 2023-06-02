97884dc2fda993dc92c70f160bd942ec331ac0d5
JonasDriessen0 <114576261+JonasDriessen0@users.noreply.github.com>
2023-06-02 10:16:45 +0200

    Meer shit gedaan

---
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace FileDirOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\programeren\2023P4ProgCs2");
+
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+
+
+            foreach (DirectoryInfo dirs in dir.GetDirectories())
+            {
+                Console.WriteLine(dirs.Name);
+            }
+        }
+    }
+}
---
+++ FileIOOpdracht/FileIOOpdracht/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace FileIOOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] content = File.ReadAllLines("leesdezefile.txt");
+
+            foreach (string line in content)
+                Console.WriteLine(line);
+
+            string[] content2 = new string[] { "bruh" };
+            File.WriteAllText("C:\\programeren\\2023P4ProgCs2\\FileIOOpdracht\\FileIOOpdracht\\bin\\Debug\\net7.0\\leesdezefile.txt", "Ik haat dit");
+
+            string[] lines = { "", "New line 1", "New line 2" };
+            File.AppendAllLines("C:\\programeren\\2023P4ProgCs2\\FileIOOpdracht\\FileIOOpdracht\\mijnnieuwefile.txt", lines);
+
+            Directory.CreateDirectory("output");
+        }
+    }
+}
